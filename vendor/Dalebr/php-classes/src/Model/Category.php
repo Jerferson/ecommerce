37014e6d98cae8816cf287293806285546f2d33b
Jerferson <jerferson.griza@tecnofit.com.br>
2020-08-31 23:16:07 -0300

    Adicionado listagem de categorias da loja

--- vendor/Dalebr/php-classes/src/Model/Category.php
+++ vendor/Dalebr/php-classes/src/Model/Category.php
@@ -37,6 +37,8 @@ class Category extends Model
             )
         );
         $this->setData($results[0]);
+
+        Category::updateFile();
     }
 
     /**
@@ -74,5 +76,25 @@ class Category extends Model
                 ":idcategory" =>  $this->getidcategory()
             )
         );
+
+        Category::updateFile();
+    }
+
+    /**
+     * Função para atualizar a lista de categorias
+     * 
+     * @return void 
+     */
+    public static function updateFile()
+    {
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/' . $row['idcategory'] . '">' . $row['descategory'] . '</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html));
     }
 }
